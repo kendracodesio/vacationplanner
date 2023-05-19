f2ac638d7b19b99c6ab89855c084eebd886e795e
Kendra Reynolds <krey214@wgu.edu>
2023-05-19 00:37:32 -0700

    Adds VacationList activity to the UI and color scheme and theme changes

---
+++ app/src/main/java/com/example/vacationplanner/UI/VacationList.java
@@ -0,0 +1,16 @@
+package com.example.vacationplanner.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vacationplanner.R;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_list);
+    }
+}
